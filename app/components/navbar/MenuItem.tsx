53dc5fffb36e9fc9b3670fcfb55af1c6883d2e33
azvonsov <azvontsov@gmail.co>
2023-08-10 18:17:06 -0400

    z-10

--- app/components/navbar/MenuItem.tsx
+++ app/components/navbar/MenuItem.tsx
@@ -1,28 +1,19 @@
 'use client';
 
 interface MenuItemProps {
-  onClick: () => void;
+  onClick?: () => void;
   label: string;
 }
 
-const MenuItem: React.FC<MenuItemProps> = ({
-  onClick,
-  label
-}) => {
-  return ( 
-    <div 
-      onClick={onClick} 
-      className="
-        px-4 
-        py-3 
-        hover:bg-neutral-100 
-        transition
-        font-semibold
-      "
+const MenuItem: React.FC<MenuItemProps> = ({ onClick, label }) => {
+  return (
+    <div
+      onClick={onClick}
+      className="px-4 py-3 hover:bg-neutral-100 transition font-semibold bg-white z-10"
     >
       {label}
     </div>
-   );
-}
- 
+  );
+};
+
 export default MenuItem;
